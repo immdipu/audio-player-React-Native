9d7f3947ca3cfe866fef2ce410307aa1d95c1370
Dipu <dipuchaurasiya91@gmail.com>
2023-10-31 23:53:07 +0545

    animation updated

--- App.tsx
+++ App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {Home, Album, Artist} from './src/screens';
-import {View} from 'react-native';
 
 import Player from './src/player/Player';
 
